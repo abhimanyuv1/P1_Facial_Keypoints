36eb5f41cea934af52c38160e9d5bd9563dbeb35
Sweta Vishwakarma <swetav@Swetas-MacBook-Air.local>
2022-09-11 15:47:21 +0530

    First try

--- models.py
+++ models.py
@@ -20,8 +20,13 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        
+        # 224x224x1 image size input
+        self.conv1 = nn.Conv2d(1, 128, 5)
+        self.pool = nn.MaxPool2d(2,2)
+        self.conv2 = nn.Conv2d(128, 256, 3)
+        self.fc1 = nn.Linear(256*54*54, 1024)
+        self.fc1_dropout = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(1024, 136)
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
@@ -31,7 +36,12 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc1_dropout(x)
+        x = self.fc2(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
